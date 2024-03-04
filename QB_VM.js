cbb125165a0202d9cd6d237e4b16cd866695934a
16372 <1637252923@qq.com>
2024-03-04 23:14:32 +0800

    测试修改

--- QB_VM.js
+++ QB_VM.js
@@ -1,9 +1,10 @@
 const {VM, VMScript} = require('vm2')
-
+const CodeGenerate = require('./node_tools/CodeGenerate.js')
 
 module.exports = {
-    start(code){
+    start(path_){
         // 用于用户来加载把代码放到vm2进行加载
+        let code = CodeGenerate(path_)
         let vm = new VM();
         return vm.run(new VMScript(code, `${__dirname}/temp.js`)); // VMScript有两个重要的参数：第一个是执行的代码，第二个是运行之后的路径，也就是在index.js中运行之后，会生成一个temp.js，temp.js存的是code的代码
     }
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-const qbvm = require('./QB_VM.js')
-qbvm.start('debugger;')
+const qbvm = require('./QB_VM.js');
+qbvm.start(`${__dirname}/vm_test_script/test.js`);
